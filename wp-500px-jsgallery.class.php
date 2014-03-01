6f8e287b6623f27e3bfaeeb457623400dc611601
mic <m@micz.it>
2014-03-01 14:59:27 +0100

    updated plugin info link in source files

--- wp-500px-jsgallery.class.php
+++ wp-500px-jsgallery.class.php
@@ -1,6 +1,6 @@
 <?
 /* Copyright 2014 Mic (email: m@micz.it)
-Plugin Info: http://micz.it
+Plugin Info: http://micz.it/wordpress-plugin-500px-jsgallery/
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as
