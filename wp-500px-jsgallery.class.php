72e19244099c31f457bb0703fa8a874eb904f247
Mic <m@micz.it>
2014-03-03 15:53:49 +0100

    version set to 1.1alpha

--- wp-500px-jsgallery.class.php
+++ wp-500px-jsgallery.class.php
@@ -29,7 +29,7 @@ if (!class_exists('WP500pxjsGallery')) {
 	  public $that;
 	  public $scripts_loaded;
 	  
-	  const version='1.0.3';
+	  const version='1.1alpha';
 	  
 	  //Options constants
 	  const _500px_user='_500px_user';
