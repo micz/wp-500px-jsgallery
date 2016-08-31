45b447476b7fed781b172013bf376ea1ac49391b
mic <m@micz.it>
2016-08-31 20:45:32 +0200

    i18n improved

--- wp-500px-jsgallery.class.php
+++ wp-500px-jsgallery.class.php
@@ -60,7 +60,7 @@ if (!class_exists('WP500pxjsGallery')) {
         add_shortcode('jsg500px', array($that,'getShortcode'));
         add_filter('plugin_action_links_'.plugin_basename(___FILE___),array($that,'add_settings_link'));
         add_filter('plugin_row_meta',array($that,'add_plugin_desc_links'),10,2);
-        load_plugin_textdomain('wp5jsgal',false,basename(dirname(___FILE___)).'/lang/');
+        load_plugin_textdomain('wp-500px-jsgallery',false,basename(dirname(___FILE___)).'/lang/');
         $this->scripts_loaded=false;
 	  }
 
@@ -81,75 +81,75 @@ if (!class_exists('WP500pxjsGallery')) {
 	  public function register_settings(){
 	    global $that;
 	    register_setting('wp5jsgal_options','wp5jsgal_options',array($that,'options_validate'));
-  	  	add_settings_section('wp5jsgal_main', esc_html__('Main Settings','wp5jsgal'), array($that,'main_section_text'), 'wp5jsgal_settings_page');
-	    add_settings_field('wp5jsgal_user',esc_html__('500px User','wp5jsgal'),null,'wp5jsgal_settings_page','default');
+  	  	add_settings_section('wp5jsgal_main', esc_html__('Main Settings','wp-500px-jsgallery'), array($that,'main_section_text'), 'wp5jsgal_settings_page');
+	    add_settings_field('wp5jsgal_user',esc_html__('500px User','wp-500px-jsgallery'),null,'wp5jsgal_settings_page','default');
 	  }
 
 	  public function admin_add_page(){
 	    global $that;
-      add_options_page(esc_html__('WP 500px jsGallery Settings','wp5jsgal'),esc_html__('WP 500px jsGallery','wp5jsgal'), 'manage_options', 'wp5jsgal_settings_page', array($that,'output_settings_page'));
+      add_options_page(esc_html__('WP 500px jsGallery Settings','wp-500px-jsgallery'),esc_html__('WP 500px jsGallery','wp-500px-jsgallery'), 'manage_options', 'wp5jsgal_settings_page', array($that,'output_settings_page'));
     }
 
     public function main_section_text() {
       $output='<p>';
-      $output.='<b>'.esc_html__('How to use this plugin:','wp5jsgal').'</b><br/>';
-      $output.=esc_html__('1. Set the 500px username and save the changes.','wp5jsgal').'<br/>';
-      $output.=esc_html__('2. Use the [jsg500px] shortcode in the page you want to show the 500px gallery on.','wp5jsgal').'<br/>';
-      $output.=esc_html__('3. (Optional) Use the "user500px" shortcode param to set a different username for a single page.','wp5jsgal').'<br/>';
+      $output.='<b>'.esc_html__('How to use this plugin:','wp-500px-jsgallery').'</b><br/>';
+      $output.=esc_html__('1. Set the 500px username and save the changes.','wp-500px-jsgallery').'<br/>';
+      $output.=esc_html__('2. Use the [jsg500px] shortcode in the page you want to show the 500px gallery on.','wp-500px-jsgallery').'<br/>';
+      $output.=esc_html__('3. (Optional) Use the "user500px" shortcode param to set a different username for a single page.','wp-500px-jsgallery').'<br/>';
       $output.='</p>';
       echo $output;
     }
 
 	  public function output_settings_page(){
 ?><div>
-<h2><?_e('WP 500px jsGallery Settings','wp5jsgal');?></h2>
-<?esc_html_e('Modify here the plugin\'s options.','wp5jsgal');?><br/>
-<b><?esc_html_e('The only mandatory option is the 500px username.','wp5jsgal');?></b>
-<br/><?esc_html_e('You can also use a custom CSS file to modify the 500px gallery look and feel.','wp5jsgal');?> (<a href="<?=self::url_custom_css_info;?>" target="_blank"><?esc_html_e('More info on custom CSS','wp5jsgal');?></a>)
+<h2><?_e('WP 500px jsGallery Settings','wp-500px-jsgallery');?></h2>
+<?esc_html_e('Modify here the plugin\'s options.','wp-500px-jsgallery');?><br/>
+<b><?esc_html_e('The only mandatory option is the 500px username.','wp-500px-jsgallery');?></b>
+<br/><?esc_html_e('You can also use a custom CSS file to modify the 500px gallery look and feel.','wp-500px-jsgallery');?> (<a href="<?=self::url_custom_css_info;?>" target="_blank"><?esc_html_e('More info on custom CSS','wp-500px-jsgallery');?></a>)
 <form action="options.php" method="post">
 <?php settings_fields('wp5jsgal_options');?>
 <?php //$options = get_option('wp5jsgal_options'); // Using $this->options?>
 <?php do_settings_sections('wp5jsgal_settings_page');?>
 <table class="form-table">
-    <tr valign="top"><th scope="row"><?esc_html_e('500px Username','wp5jsgal');?></th>
+    <tr valign="top"><th scope="row"><?esc_html_e('500px Username','wp-500px-jsgallery');?></th>
         <td><input name="wp5jsgal_options[<?=self::_500px_user?>]" type="text" value="<?php echo $this->options[self::_500px_user]; ?>"<?if($this->options[self::_500px_user]==''){echo 'style="border:2px solid red;"';}?>/>
-        <br/><?if($this->options[self::_500px_user]==''){echo '<span style="color:red;font-weight:bold;">'.esc_html__('You must specify a 500px username here or using the "user500px" shortcode param directly on the page!','wp5jsgal').'<span>';
+        <br/><?if($this->options[self::_500px_user]==''){echo '<span style="color:red;font-weight:bold;">'.esc_html__('You must specify a 500px username here or using the "user500px" shortcode param directly on the page!','wp-500px-jsgallery').'<span>';
         }else{
-          esc_html__('You can also specify a 500px username using the "user500px" shortcode param directly on the page.','wp5jsgal');
+          esc_html__('You can also specify a 500px username using the "user500px" shortcode param directly on the page.','wp-500px-jsgallery');
         }?></td>
     </tr>
-   <tr valign="top"><th scope="row"><?esc_html_e('Number of thumbnails per page','wp5jsgal');?></th>
+   <tr valign="top"><th scope="row"><?esc_html_e('Number of thumbnails per page','wp-500px-jsgallery');?></th>
         <td><input type="text" name="wp5jsgal_options[<?=self::_page_thumbs?>]" value="<?php echo $this->options[self::_page_thumbs]; ?>"/></td>
     </tr>
-   <tr valign="top"><th scope="row"><?esc_html_e('Thumbnails dimensions','wp5jsgal');?></th>
-        <td><?esc_html_e('Height','wp5jsgal');?> <input type="text" name="wp5jsgal_options[<?=self::_thumb_h?>]" value="<?php echo $this->options[self::_thumb_h]; ?>"/> <?esc_html_e('px','wp5jsgal');?><br/>
-        <?esc_html_e('Width','wp5jsgal');?> <input type="text" name="wp5jsgal_options[<?=self::_thumb_w?>]" value="<?php echo $this->options[self::_thumb_w]; ?>"/> <?esc_html_e('px','wp5jsgal');?><br/>
-        <?esc_html_e('The dimensions could be set with the custom CSS, instead of with these options.','wp5jsgal');?><br/>
-        <?esc_html_e('You can also set only one dimension, the other will be proportional.','wp5jsgal');?></td>
+   <tr valign="top"><th scope="row"><?esc_html_e('Thumbnails dimensions','wp-500px-jsgallery');?></th>
+        <td><?esc_html_e('Height','wp-500px-jsgallery');?> <input type="text" name="wp5jsgal_options[<?=self::_thumb_h?>]" value="<?php echo $this->options[self::_thumb_h]; ?>"/> <?esc_html_e('px','wp-500px-jsgallery');?><br/>
+        <?esc_html_e('Width','wp-500px-jsgallery');?> <input type="text" name="wp5jsgal_options[<?=self::_thumb_w?>]" value="<?php echo $this->options[self::_thumb_w]; ?>"/> <?esc_html_e('px','wp-500px-jsgallery');?><br/>
+        <?esc_html_e('The dimensions could be set with the custom CSS, instead of with these options.','wp-500px-jsgallery');?><br/>
+        <?esc_html_e('You can also set only one dimension, the other will be proportional.','wp-500px-jsgallery');?></td>
     </tr>
-   <tr valign="top"><th scope="row"><?esc_html_e('Image dimensions','wp5jsgal');?></th>
-        <td><?esc_html_e('Max Height','wp5jsgal');?> <input type="text" name="wp5jsgal_options[<?=self::_image_h?>]" value="<?php echo $this->options[self::_image_h]; ?>"/> <?esc_html_e('px','wp5jsgal');?><br/>
-        <?esc_html_e('Max Width','wp5jsgal');?> <input type="text" name="wp5jsgal_options[<?=self::_image_w?>]" value="<?php echo $this->options[self::_image_w]; ?>"/> <?esc_html_e('px','wp5jsgal');?><br/>
-        <?esc_html_e('The dimensions could be set with the custom CSS, instead of with these options.','wp5jsgal');?><br/>
-        <?esc_html_e('You can also set only one dimension, the other will be proportional.','wp5jsgal');?></td>
+   <tr valign="top"><th scope="row"><?esc_html_e('Image dimensions','wp-500px-jsgallery');?></th>
+        <td><?esc_html_e('Max Height','wp-500px-jsgallery');?> <input type="text" name="wp5jsgal_options[<?=self::_image_h?>]" value="<?php echo $this->options[self::_image_h]; ?>"/> <?esc_html_e('px','wp-500px-jsgallery');?><br/>
+        <?esc_html_e('Max Width','wp-500px-jsgallery');?> <input type="text" name="wp5jsgal_options[<?=self::_image_w?>]" value="<?php echo $this->options[self::_image_w]; ?>"/> <?esc_html_e('px','wp-500px-jsgallery');?><br/>
+        <?esc_html_e('The dimensions could be set with the custom CSS, instead of with these options.','wp-500px-jsgallery');?><br/>
+        <?esc_html_e('You can also set only one dimension, the other will be proportional.','wp-500px-jsgallery');?></td>
     </tr>
-   <tr valign="top"><th scope="row"><?esc_html_e('Gallery page','wp5jsgal');?></th>
+   <tr valign="top"><th scope="row"><?esc_html_e('Gallery page','wp-500px-jsgallery');?></th>
         <td><input type="text" name="wp5jsgal_options[<?=self::_pages?>]" value="<?php echo $this->options[self::_pages]; ?>"/>
-        <br/><?esc_html_e('To optimize your website loading times, you could write here the pages id or permalink on which you have activated the 500px gallery with the shortcode.','wp5jsgal');?><br/>
-        <?esc_html_e('The ids or permalinks must be comma separated and can be mixed.','wp5jsgal');?><br/>
-        <?esc_html_e('All the styles and scripts needed by this plugin will be loaded only on those pages.','wp5jsgal');?></td>
+        <br/><?esc_html_e('To optimize your website loading times, you could write here the pages id or permalink on which you have activated the 500px gallery with the shortcode.','wp-500px-jsgallery');?><br/>
+        <?esc_html_e('The ids or permalinks must be comma separated and can be mixed.','wp-500px-jsgallery');?><br/>
+        <?esc_html_e('All the styles and scripts needed by this plugin will be loaded only on those pages.','wp-500px-jsgallery');?></td>
     </tr>
-   <tr valign="top"><th scope="row"><?esc_html_e('Use CSS version 1','wp5jsgal');?></th>
-        <td><input type="checkbox" name="wp5jsgal_options[<?=self::_force_css_v1?>]" value="1"<?php if($this->options[self::_force_css_v1]==1){echo ' checked="checked"';} ?>"/> <?esc_html_e('Check this option if you want to use the old CSS version 1.','wp5jsgal');?><br/>
-        <?esc_html_e('From version 2.0 the CSS is responsive and the thumbnails are displayed under the image.','wp5jsgal');?><br/>
-        <a href="<?=self::url_force_css_v1;?>" target="_blank"><?esc_html_e('More info on the old CSS version 1','wp5jsgal');?></a></td>
+   <tr valign="top"><th scope="row"><?esc_html_e('Use CSS version 1','wp-500px-jsgallery');?></th>
+        <td><input type="checkbox" name="wp5jsgal_options[<?=self::_force_css_v1?>]" value="1"<?php if($this->options[self::_force_css_v1]==1){echo ' checked="checked"';} ?>"/> <?esc_html_e('Check this option if you want to use the old CSS version 1.','wp-500px-jsgallery');?><br/>
+        <?esc_html_e('From version 2.0 the CSS is responsive and the thumbnails are displayed under the image.','wp-500px-jsgallery');?><br/>
+        <a href="<?=self::url_force_css_v1;?>" target="_blank"><?esc_html_e('More info on the old CSS version 1','wp-500px-jsgallery');?></a></td>
     </tr>
-   <tr valign="top"><th scope="row"><?esc_html_e('Exclusive custom CSS','wp5jsgal');?></th>
-        <td><input type="checkbox" name="wp5jsgal_options[<?=self::_only_custom_css?>]" value="1"<?php if($this->options[self::_only_custom_css]==1){echo ' checked="checked"';} ?>"/> <?esc_html_e('Check this option if you want to load only your custom CSS and not the default one before your one.','wp5jsgal');?><br/>
-        <a href="<?=self::url_custom_css_info;?>" target="_blank"><?esc_html_e('More info on custom CSS','wp5jsgal');?></a></td>
+   <tr valign="top"><th scope="row"><?esc_html_e('Exclusive custom CSS','wp-500px-jsgallery');?></th>
+        <td><input type="checkbox" name="wp5jsgal_options[<?=self::_only_custom_css?>]" value="1"<?php if($this->options[self::_only_custom_css]==1){echo ' checked="checked"';} ?>"/> <?esc_html_e('Check this option if you want to load only your custom CSS and not the default one before your one.','wp-500px-jsgallery');?><br/>
+        <a href="<?=self::url_custom_css_info;?>" target="_blank"><?esc_html_e('More info on custom CSS','wp-500px-jsgallery');?></a></td>
     </tr>
 </table>
-<input name="Submit" class="button button-primary" type="submit" value="<?php esc_attr_e('Save Changes','wp5jsgal');?>"/>
+<input name="Submit" class="button button-primary" type="submit" value="<?php esc_attr_e('Save Changes','wp-500px-jsgallery');?>"/>
 </form></div>
 	  <?}
 
@@ -182,13 +182,13 @@ if (!class_exists('WP500pxjsGallery')) {
 
 //Plugin admin page
   function add_settings_link($links){
-    $links[] = '<a href="options-general.php?page=wp5jsgal_settings_page">'.__('Settings','wp5jsgal').'</a>';
+    $links[] = '<a href="options-general.php?page=wp5jsgal_settings_page">'.__('Settings','wp-500px-jsgallery').'</a>';
 	  return $links;
   }
 
   function add_plugin_desc_links($links,$file){
     if(strpos($file,plugin_basename(___FILE___))!==false){
-      $links = array_merge($links,array('<a href="'.self::url_donate.'">'.__('Donate','wp5jsgal').'</a>'));
+      $links = array_merge($links,array('<a href="'.self::url_donate.'">'.__('Donate','wp-500px-jsgallery').'</a>'));
     }
     return $links;
   }
@@ -202,14 +202,14 @@ if (!class_exists('WP500pxjsGallery')) {
 	    $user500px=trim(wp_filter_nohtml_kses($user500px));
       if($this->scripts_loaded==false){ //the user is not loading the scripts in this page
         if(current_user_can('manage_options')){ //the current user can manage options
-          return '<p><span style="color:red;font-weight:bold;">'.esc_html__('You\'ve set the wrong page id or permalink in the plugin settings, so the WP 500px jsGallery Plugin scripts are not loaded in this page!','wp5jsgal').'<span></p>';
+          return '<p><span style="color:red;font-weight:bold;">'.esc_html__('You\'ve set the wrong page id or permalink in the plugin settings, so the WP 500px jsGallery Plugin scripts are not loaded in this page!','wp-500px-jsgallery').'<span></p>';
         }else{ //the current can NOT manage options
           return '';
         }
       }
       if($user500px==''){ //no 500px username set
         if(current_user_can('manage_options')){ //the current user can manage options
-          return '<p><span style="color:red;font-weight:bold;">'.esc_html__('To use the WP 500px jsGallery Plugin shortcode, you must specify a 500px username in the plugin settings or using the "user500px" shortcode param!','wp5jsgal').'<span></p>';
+          return '<p><span style="color:red;font-weight:bold;">'.esc_html__('To use the WP 500px jsGallery Plugin shortcode, you must specify a 500px username in the plugin settings or using the "user500px" shortcode param!','wp-500px-jsgallery').'<span></p>';
         }else{ //the current can NOT manage options
           return '';
         }
@@ -222,9 +222,9 @@ wp5jsgal_options["_500px_user"]="'.$user500px.'";
 /* ]]> */
 </script>';
         }
-        $output.='<div id="wp500pxnojs" style="color:red;font-weight:bold;">'.esc_html__('The WP 500px jsGallery is not working because Javascript is disabled.','wp5jsgal').'</div>
+        $output.='<div id="wp500pxnojs" style="color:red;font-weight:bold;">'.esc_html__('The WP 500px jsGallery is not working because Javascript is disabled.','wp-500px-jsgallery').'</div>
         <div id="wp500pxgallery-main">
-            <div id="wp500pxloading" class="loader" style="display:none;"><img src="'.plugins_url('img/loadingAnimation.gif',___FILE___).'" width="208" height="13"/><br/>'.esc_html__('Loading images...','wp5jsgal').'</div>
+            <div id="wp500pxloading" class="loader" style="display:none;"><img src="'.plugins_url('img/loadingAnimation.gif',___FILE___).'" width="208" height="13"/><br/>'.esc_html__('Loading images...','wp-500px-jsgallery').'</div>
             <div id="wp500pxgallery" class="wp500pxcontent">
             <div id="wp500pxcontrols" class="wp500pxcontrols"></div>
             <div class="slideshow-container">
@@ -235,7 +235,7 @@ wp5jsgal_options["_500px_user"]="'.$user500px.'";
           </div>
           <div id="wp500pxthumbs" class="wp500pxnavigation"><ul class="thumbs noscript">';
         $output.='</ul><div class="wp500pxgallery-footer">&nbsp;</div></div><div class="wp500pxgallery-footer">&nbsp;</div>';
-        $output.='<div id="wp500pxlinkprofile">'.esc_html__('Browse all images on 500px:','wp5jsgal').' <a href="http://500px.com/'.$user500px.'/" target="_blank">http://500px.com/'.$user500px.'/</a></div>';
+        $output.='<div id="wp500pxlinkprofile">'.esc_html__('Browse all images on 500px:','wp-500px-jsgallery').' <a href="http://500px.com/'.$user500px.'/" target="_blank">http://500px.com/'.$user500px.'/</a></div>';
         $output.='</div>';
       } //END if 500px username set
       return $output;
@@ -244,13 +244,13 @@ wp5jsgal_options["_500px_user"]="'.$user500px.'";
 
 	 public function getJsLang(){
 	    $jslang=array();
-      $jslang['image_link_desc']=esc_attr__('See this image at full size on 500px','wp5jsgal');
-      $jslang['gal_playLinkText']=esc_attr__('Play','wp5jsgal');
-      $jslang['gal_pauseLinkText']=esc_attr__('Pause','wp5jsgal');
-      $jslang['gal_prevLinkText']=esc_attr__('Previous','wp5jsgal');
-      $jslang['gal_nextLinkText']=esc_attr__('Next','wp5jsgal');
-      $jslang['gal_nextPageLinkText']=esc_attr__('Next &rsaquo;','wp5jsgal');
-      $jslang['gal_prevPageLinkText']=esc_attr__('&lsaquo; Prev','wp5jsgal');
+      $jslang['image_link_desc']=esc_attr__('See this image at full size on 500px','wp-500px-jsgallery');
+      $jslang['gal_playLinkText']=esc_attr__('Play','wp-500px-jsgallery');
+      $jslang['gal_pauseLinkText']=esc_attr__('Pause','wp-500px-jsgallery');
+      $jslang['gal_prevLinkText']=esc_attr__('Previous','wp-500px-jsgallery');
+      $jslang['gal_nextLinkText']=esc_attr__('Next','wp-500px-jsgallery');
+      $jslang['gal_nextPageLinkText']=esc_attr__('Next &rsaquo;','wp-500px-jsgallery');
+      $jslang['gal_prevPageLinkText']=esc_attr__('&lsaquo; Prev','wp-500px-jsgallery');
       return $jslang;
 	 }
 
