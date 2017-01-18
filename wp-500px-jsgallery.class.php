5e9e26e4fc89c96d293521f214298333f65a76ef
mic <m@micz.it>
2017-01-18 23:49:13 +0100

    version set to 2.1

--- wp-500px-jsgallery.class.php
+++ wp-500px-jsgallery.class.php
@@ -29,7 +29,7 @@ if (!class_exists('WP500pxjsGallery')) {
 	  public $that;
 	  public $scripts_loaded;
 
-	  const version='2.1alpha';
+	  const version='2.1';
 	  const db_version=2;
 
 	  //URL constants
